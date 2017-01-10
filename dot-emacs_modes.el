0aec27abd4642f4de82e7e46f55539bfd0075924
David Andrzejewski <david@sumologic.com>
2017-01-10 10:11:09 -0800

    add emacs modes

---
+++ dot-emacs_modes.el
@@ -0,0 +1,4 @@
+(setq auto-mode-alist (cons '("\\.avpr$" . javascript-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.json" . javascript-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.avdl" . javascript-mode) auto-mode-alist))
